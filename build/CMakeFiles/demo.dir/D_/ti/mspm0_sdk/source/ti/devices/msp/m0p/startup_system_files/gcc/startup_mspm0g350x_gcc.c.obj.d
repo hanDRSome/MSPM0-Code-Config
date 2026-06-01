67017edddd7d5eddd33bd2969a326bd3043fd6be
hanDRSome <dourenshuai18@gmail.com>
2026-06-01 22:40:19 +0800

    intial-config

---
+++ build/CMakeFiles/demo.dir/D_/ti/mspm0_sdk/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g350x_gcc.c.obj.d
@@ -0,0 +1,9 @@
+CMakeFiles/demo.dir/D_/ti/mspm0_sdk/source/ti/devices/msp/m0p/startup_system_files/gcc/startup_mspm0g350x_gcc.c.obj: \
+ D:\ti\mspm0_sdk\source\ti\devices\msp\m0p\startup_system_files\gcc\startup_mspm0g350x_gcc.c \
+ D:/Env/msys2/ucrt64/lib/gcc/arm-none-eabi/13.3.0/include/stdint.h \
+ D:/Env/msys2/ucrt64/arm-none-eabi/include/stdint.h \
+ D:/Env/msys2/ucrt64/arm-none-eabi/include/machine/_default_types.h \
+ D:/Env/msys2/ucrt64/arm-none-eabi/include/sys/features.h \
+ D:/Env/msys2/ucrt64/arm-none-eabi/include/_newlib_version.h \
+ D:/Env/msys2/ucrt64/arm-none-eabi/include/sys/_intsup.h \
+ D:/Env/msys2/ucrt64/arm-none-eabi/include/sys/_stdint.h
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "ti_msp_dl_config.h"
+int main() {
+    SYSCFG_DL_init();
+    while (1) {
+        DL_GPIO_togglePins(LED_PORT, LED_PIN_0_PIN);
+        delay_cycles(CPUCLK_FREQ );
+    }
+    return 0;
+}
